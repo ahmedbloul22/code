603effde636e998ada377698e85bae0cb1721303
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:12:57 +0000

    Add files via upload

---
+++ greeclass.java
@@ -0,0 +1,18 @@
+package greepack;
+
+import java.util.Scanner;
+
+public class greeclass {
+	
+	
+	
+	static Scanner reader=new Scanner(System.in);
+	public static void main(string[] args) {
+
+    
+    
+	
+    }	
+    }
+	
+	
