c933d2e80c7d9ea99378dda7dd6a163a88199f67
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:31:04 +0000

    Add files via upload

---
+++ hi.java
@@ -0,0 +1,30 @@
+package pack;
+
+
+import java.util.Scanner;
+public class hi {
+	
+	
+	
+
+	 static  int sum(int n1,int n2){
+		    	int r=n1+n2;
+		return(r); 	
+		    
+	   }
+	
+	
+	//static Scanner reader= new Scanner(System.in);
+	 	public static void main(String[] args) {
+		
+	
+		
+ int result=   sum(3,4);
+		
+ System.out.println(result);
+ 
+	}
+	
+	
+
+}
