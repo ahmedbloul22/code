1f6a4c0014760fcb33ac8546561d8cc18c397369
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:40:10 +0000

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,20 @@
+package pack;
+
+import java.util.Scanner;
+
+public class hello {
+
+
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+  
+    	
+    	int a=0;
+    	while((a>=0)&& (a<=5))
+    	{
+        System.out.println(a);
+
+    a++;
+    	}
+}
+}
