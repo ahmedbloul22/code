cf2419127d9279fe3fbcaebedce9672286d43a57
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:37:16 +0000

    Add files via upload

---
+++ clss.java
@@ -0,0 +1,26 @@
+package s.tst;
+
+import java.util.Scanner;
+
+public class clss {
+
+
+
+static Scanner reader = new Scanner (System.in);
+public static void main(String[] args) {
+ 
+	  System.out.println("pc1=");
+	   int pc1=reader.nextInt();
+	   System.out.println("pc2=");
+	   int pc2=reader.nextInt();
+	   System.out.println("pc3=");
+	   int pc3=reader.nextInt();
+	   int result=pc1*pc2+pc3;
+	   System.out.println("result=");
+	   System.out.println(result);
+	   
+	    }
+	}
+    
+    
+   
