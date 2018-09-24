f5eddae94ba34cae8061bff9cc0c41f6de16eecb
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:27:56 +0000

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Book {
+
+       static Scanner reader = new Scanner (System.in);	
+       public static void main(String[] args){
+    	   
+      
+        
+    	
+       String name="ahmed";
+       int age=15;
+       String school="excellence high school";
+       String livein="Nouakchott";
+       
+    	 
+    	
+    	
+       System.out.println("name:"+name);
+       System.out.println("age:"+age);
+       System.out.println("school:"+school);
+       System.out.println("city:"+livein);
+
+    	
+    	
+    	
+       }
+       }
