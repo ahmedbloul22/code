527a34e4d8cb2574affbec36758a80975b026676
ahmedbloul22 <42880315+ahmedbloul22@users.noreply.github.com>
2018-09-24 15:27:13 +0000

    Add files via upload

---
+++ games.java
@@ -0,0 +1,46 @@
+package game;
+
+import java.util.Scanner;
+
+public class games {
+
+	
+	
+   static  int sum(int n1,int n2){
+	
+		
+		return(n1+n2);
+		
+		
+		
+	}
+	
+int sum(int n1,int n2,int n3){
+	
+	return(n1+n2+n3);
+	
+	
+	
+}
+	
+  // static Scanner reader=new Scanner(System.in);
+public static void main(String[] args) {
+	
+	int r2=new games().sum(4, 8);
+	int r3=new games().sum(4, 8, 6);
+		
+System.out.println(r2);
+System.out.println(r3);
+
+
+
+
+
+
+}
+
+
+}
+	
+	
+	
